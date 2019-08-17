8829376b6b8ee709b3942b8aa7e8ed30ea21ab7b
DONGGEON LIM <ooqwe486@gmail.com>
2019-08-17 14:07:05 +0000

    Add base settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/graphql.ts
@@ -0,0 +1,9 @@
+
+/** ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+export abstract class IQuery {
+}
